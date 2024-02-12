c0fc3d7bec008ae87dd10a90fc2c7a6656edebbe
takesixteen-h <156801866+takesixteen-h@users.noreply.github.com>
2024-02-12 10:16:56 +0000

    document function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,3 +1,8 @@
+/**
+ * this is a simple function that returns a promise that resolves after a given number of milliseconds
+ * @param ms - number of milliseconds to wait
+ */
 export async function delay(ms: number) {
+
   return new Promise((resolve) => setTimeout(resolve, ms));
 }
